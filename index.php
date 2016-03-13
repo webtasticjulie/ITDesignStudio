b67339d9f1c97df0b0aee921a60ad08c3f291ceb
Julie Beck <webtasticjulie@gmail.com>
2016-03-12 19:14:42 -0500

    Added files via upload
    Added session start and login feature to index page. Need to review / remove inline css

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start('tasks');?>
+
 <!DOCTYPE html>
 
 <html>
@@ -26,19 +28,34 @@
     
    <?php include('header.php');?>
     <div id="maincontent">
+       
     <div id="titlebar">
     <h3>Welcome</h3>
         
     <hr>
+        <span style="width: 30%; float: left; padding: 1em;">
+        <?php if (!isset($_SESSION['username'])){ ?>
+        <form name="username" method="post" action="login.php" transition="flip">
+            <label for="username">Username:<input type="text" name="username" id="username" maxlength="50"/></label><br/>
+            <label for="password">Password:<input type="password" name="password" id="password"/></label>
+            <input type="submit" value="Submit"/>
+
+        </form>
+        <?php }else{
+        echo "Welcome ".$_SESSION['username'];
+                }
+                ?>
+        </span>
          <strong>Welcome! Looking for service providers? Need help with PHP? We have a community of individuals listed on our site offering services from PHP tutoring to HTML.  </strong>
         <p><a href="registration.php" data-transition="slide">Register</a> for our site!</p>
-        <h4>Admins</h4><hr/>
+        <h4>Admins</h4>
         <ul>
             <li>Julie Beck</li>
             <li>Alfonso Madrid</li>
             <li>Pakorn</li>
             <li>Don</li>
         </ul>
+
   </div>
 
  
