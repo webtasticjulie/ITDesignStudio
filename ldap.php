6450518c955b83c41e686d1e3ad6fc7c1f5f7acb
Julie Beck <webtasticjulie@gmail.com>
2016-03-20 16:00:07 -0400

    Added files via upload

---
+++ ldap.php
@@ -0,0 +1,7 @@
+<?php
+
+//administrator credentials in order to add new entries  these really should be in an external file somewhere not hardcoded into the logic!!
+$ldaprdn = "cn=manager,dc=designstudio1,dc=com";
+$ldappass = "my*password"; // associated password
+
+?>
